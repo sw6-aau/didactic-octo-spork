562c68048d210478a337b876d3f75dd42d469af3
Peter K <pkjar16@student.aau.dk>
2020-04-21 13:46:48 +0200

    POG WORKS

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /working/LSTnet-demo/
 RUN mkdir log/ save/ data/
 RUN wget -O data/exchange_rate.txt https://sembrik.s3.eu-west-2.amazonaws.com/sw6/exchange_rate.txt
 RUN chmod +x stock-cpu.sh
-COPY . /app
-WORKDIR /app
+COPY . /working/LSTnet-demo/
+RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 from subprocess import Popen, PIPE
+import os
 
 app = Flask(__name__)
 
-@app.route('/train')
+@app.route('/train', methods=['GET'])
 def train():
     build_id = request.args.get("build_id")
+    build_data = request.args.get("build_data")
+    build_model = request.args.get("build_model")
+    
     horizon = request.args.get("horizon")
     dropout = request.args.get("dropout")
     skip_rnn = request.args.get("skip_rnn")
@@ -18,17 +22,30 @@ def train():
     af_output = request.args.get("af_output")
     af_ae = request.args.get("af_ae")
     
-    process = Popen(['python2 main.py', "--data " + build_id, "--save " + build_id, "--hidCNN " + ae_hid_cnn, "--hidRNN " + hid_rnn, "--L1Loss False", "--Output_fun None"], stdout=PIPE, stderr=PIPE)
+    process = Popen(['python2 main.py', "--data " + build_data, "--save " + build_model, "--hidCNN " + ae_hid_cnn, "--hidRNN " + hid_rnn, "--L1Loss False", "--Output_fun None"], stdout=PIPE, stderr=PIPE)
     process.wait() # Wait for training to complete.
     
     return build_id
 
 
-@app.route('/predict')
+@app.route('/predict', methods=['GET'])
 def predict():
     build_id = request.args.get("build_id")
     
-    process = Popen(['python2 predict.py', "--data " + build_id, "--save " + build_id, "--hidCNN " + ae_hid_cnn, "--hidRNN " + hid_rnn, "--L1Loss False", "--Output_fun None"], stdout=PIPE, stderr=PIPE)
-    process.wait() # Wait for training to complete.
+    #os.system('python2 predict.py', "--data " + build_id, "--save " + build_id, "--L1Loss False", "--Output_fun None")
+   # process.wait() # Wait for training to complete.
     
     return build_id
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "works"
+
+@app.route('/default', methods=['GET'])
+def default():
+    os.system('python2 main.py --data exchange_rate.txt --save exchange_rate.pt --hidCNN 50 --hidRNN 50 --epochs 10 --L1Loss False --output_fun None')
+    return "done"
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
